bed8770bb49d9b5afe1df5acee121253f537ddd1
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-12-23 22:57:55 +0200

    audit contactId

--- models/contact.js
+++ models/contact.js
@@ -17,7 +17,7 @@ const contactSchema = new Schema({
     type: Boolean,
     default: false,
   },
-});
+}, { versionKey: false, timestamps: true });
 
 contactSchema.post("save", handleMongooseError);
 
