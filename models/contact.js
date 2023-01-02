677703fbeec6a8f8d83f71ae0e36975cc478bbfd
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-02 19:51:03 +0200

    add schemas

--- models/contact.js
+++ models/contact.js
@@ -17,6 +17,10 @@ const contactSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    },
 }, { versionKey: false, timestamps: true });
 
 contactSchema.post("save", handleMongooseError);
