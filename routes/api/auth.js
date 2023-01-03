bb56c50e215eb953b23e1a387dd6f48574487dff
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-03 20:42:03 +0200

    add logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,5 +10,6 @@ const router = express.Router()
 router.post("/register", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register))
 router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login))
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent))
+router.post("/logout", authenticate, ctrlWrapper(ctrl.logout))
 
 module.exports = router
