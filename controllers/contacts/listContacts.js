8842d660507a11a803d0b075eb3f6e7bee609589
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-03 20:03:15 +0200

    add getCurrent

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,10 @@ const { Contact } = require("../../models/contact")
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({owner}, "-createdAt -updatedAt")
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit })
+                          .populate("owner", "name email")
   res.json(result)
 }
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,13 @@ const express = require('express')
 
 const ctrl = require('../../controllers/auth')
 const { ctrlWrapper } = require("../../helpers")
-const { validateBody } = require("../../middlewares")
+const { validateBody, authenticate } = require("../../middlewares")
 const { schemas } = require("../../models/user")
 
 const router = express.Router()
 
 router.post("/register", validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register))
 router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login))
+router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent))
 
 module.exports = router
