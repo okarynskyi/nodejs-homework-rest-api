0f5b7d49de9fe8160214694ad1159b1833547725
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-03 19:26:26 +0200

    add authenticate

--- models/contact.js
+++ models/contact.js
@@ -17,10 +17,10 @@ const contactSchema = new Schema({
     type: Boolean,
     default: false,
   },
-  // owner: {
-  //     type: SchemaTypes.ObjectId,
-  //     ref: 'user',
-  //   },
+  owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
 }, { versionKey: false, timestamps: true });
 
 contactSchema.post("save", handleMongooseError);
