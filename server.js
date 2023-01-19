74ad10d7fc210326dce53dfb8b0ea154a542f2a3
Oleh Okarynskyi <okarynskyy@gmail.com>
2023-01-19 14:00:08 +0200

    add sendEmail

--- server.js
+++ server.js
@@ -14,3 +14,26 @@ mongoose.connect(DB_HOST)
     console.log(err);
     process.exit(1);
   })
+
+
+//   const sgMail = require('@sendgrid/mail')
+// require("dotenv").config()
+
+// const { SENDGRID_API_KEY } = process.env;
+//   sgMail.setApiKey(SENDGRID_API_KEY);
+
+// const email = {
+//   to: 'tebipa3294@ukbob.com',
+//   from: 'okarynskyy@gmail.com',
+//   subject: 'Verify email',
+//   text: 'Verify email',
+//   html: '<strong>Verify email</strong>',
+// }
+// sgMail
+//   .send(email)
+//   .then(() => {
+//     console.log('Email send')
+//   })
+//   .catch((error) => {
+//     console.error(error.message)
+//   })
